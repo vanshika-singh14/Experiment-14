664796def3afd2f2515566f68a71706f0d6a9e3a
vanshika singh <vanshikajksingh1405@gmail.com>
2024-10-25 00:50:41 +0530

    Create exp14a.cpp

---
+++ exp14a.cpp
@@ -0,0 +1,30 @@
+//Vanshika Singh
+//23070123148
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+class uni
+{
+    public:
+    string uni = "Symbiosis";
+    void course()
+    {
+        cout<<"B. Tech ";
+    }
+};
+class branch: public uni
+{
+    public:
+    string branch = " Electronics and Telecommunication ";
+
+};
+
+int main()
+{
+    branch b1;
+    cout<<b1.uni<<endl;
+    b1.course();
+    cout<<b1.branch;
+}
